217aaecc60fbeb80c183d4e2a8b2ee1983e355e1
phyo <phyo@xenoptics.com>
2025-05-22 18:00:39 +0700

    🚧 Better file structure
    better file structure

---
+++ controllers/userController.js
@@ -0,0 +1,34 @@
+exports.getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is under maintainance',
+  });
+};
+
+exports.createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is under maintainance',
+  });
+};
+
+exports.getUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is under maintainance',
+  });
+};
+
+exports.updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is under maintainance',
+  });
+};
+
+exports.deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is under maintainance',
+  });
+};
