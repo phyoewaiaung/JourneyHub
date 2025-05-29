f6940d2938a965b768ef84892f74f030096ec74e
phyo <phyo@xenoptics.com>
2025-05-29 11:21:31 +0700

    🎨 MVC
    CODE REFRACTOR MVC

--- server.js
+++ server.js
@@ -1,6 +1,34 @@
+const mongoose = require('mongoose');
+
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('db success');
+  });
+
+// const testTour = new Tour({
+//   name: 'Camping',
+//   rating: 4.7,
+//   price: 498,
+// });
+
+// testTour
+//   .save()
+//   .then((doc) => console.log(doc))
+//   .catch((e) => console.log(e));
 
 const app = require('./app');
 
